1421fd4dc8702d2a4588aaae3c7875e53ef6d549
Cedric Manamela <manamelacedric@gmail.com>
2021-01-28 08:21:18 +0200

    able to send a telegram message

--- bot.js
+++ bot.js
@@ -2,7 +2,9 @@ const { Telegraf } = require('telegraf')
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.start((ctx) => ctx.reply('Welcome'))
-bot.on('text', (ctx) => ctx.reply(`Still busy, I'll get back shortly.`))
+bot.on('text', (ctx) => {
+  console.log('ctx', ctx.update.message.chat)
+  ctx.reply(`Still busy, I'll get back shortly.`)})
 bot.catch((err, ctx) => {
   console.log(`Ooops, encountered an error for ${ctx.updateType}`, err)
 })
--- index.js
+++ index.js
@@ -8,8 +8,29 @@ app.use(express.json())
 
 app.get('/', (_, res) => res.send('Server working.'))
 app.post('/form', (req, res) => {
-  const { name, email, cellphone, messsage } = req.body
-  res.json({ success: true })
+  const { name, email, cellphone, message } = req.body
+  const chatId = process.env.ID
+
+  if (!name) {
+    return res.status(500).json({
+      success: false, error: { message: 'please specify you name'}
+    })
+  }
+
+  // todo: fix the order
+  const messages = [
+    bot.telegram.sendMessage(chatId, 'New Form Submission'),
+    bot.telegram.sendMessage(chatId, `Name: ${name}`),
+    bot.telegram.sendMessage(chatId, `Contacts: ${cellphone} ${email}`),
+    bot.telegram.sendMessage(chatId, `Message: ${message}`),
+  ]
+  Promise.all(messages)
+    .then(() => res.json({ success: true }))
+    .catch((err) => {
+      console.log('SendMessage Error: ', err)
+      res.status(500).json({ success: true })
+    })
+  
 })
 
 app.listen(parseInt(process.env.PORT, 10), (err) => {
