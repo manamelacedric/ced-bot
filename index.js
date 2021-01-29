baa8e91b8d871525365dba6d8a63db00746f147c
Cedric Manamela <manamelacedric@gmail.com>
2021-01-29 20:12:07 +0200

    complete auth and logger middleware

--- index.js
+++ index.js
@@ -1,19 +1,69 @@
 require('dotenv').config()
 const express = require('express')
 const bot = require('./bot')
+const httpCodes = {
+  ok: 200,
+  badReq: 400,
+  noAuth:  403,
+  serverErr: 500,
+}
+
 
 const app = express()
 
 app.use(express.json())
 
 app.get('/', (_, res) => res.send('Server working.'))
-app.post('/form', (req, res) => {
+
+// auth middleware
+app.use((req, res, next) => {
+  const apiKey = req.headers['x-api-key']
+  if (!apiKey || apiKey !== process.env.API_KEY) {
+    return res.status(httpCodes.noAuth).json({
+      success: false,
+      error: { message: 'You are not authorised to perform this action.' }
+    })
+  }
+  return next()
+})
+
+// logger middleware
+app.use((req, _, next) => {
+  const { method, originalUrl } = req
+  const date = new Date()
+  const formatedDate = date.toLocaleString('en-ZA', { timeZone: 'Africa/Johannesburg' })
+    .replace(',', '')
+    .replace(/\//g, '-')
+  console.log(`[${formatedDate}] ${method.toUpperCase()} ${originalUrl}`)
+  return next()
+})
+
+// bot endpoin
+app.post('/', (req, res) => {
   const { name, email, cellphone, message } = req.body
   const chatId = process.env.ID
 
   if (!name) {
-    return res.status(500).json({
+    return res.status(httpCodes.badReq).json({
       success: false, error: { message: 'please specify you name'}
+    })
+  }
+
+  if (!cellphone) {
+    return res.status(httpCodes.badReq).json({
+      success: false, error: { message: 'please specify your number'}
+    })
+  }
+
+  if (!email) {
+    return res.status(httpCodes.badReq).json({
+      success: false, error: { message: 'please specify your email'}
+    })
+  }
+
+  if (!message) {
+    return res.status(httpCodes.badReq).json({
+      success: false, error: { message: 'please specify your message'}
     })
   }
 
@@ -28,11 +78,12 @@ app.post('/form', (req, res) => {
     .then(() => res.json({ success: true }))
     .catch((err) => {
       console.log('SendMessage Error: ', err)
-      res.status(500).json({ success: true })
+      res.status(httpCodes.serverErr).json({ success: true })
     })
   
 })
 
+// start server
 app.listen(parseInt(process.env.PORT, 10), (err) => {
   if(err) console.log('Failed to start the server')
   console.log('Server listening on port ' + process.env.PORT)
